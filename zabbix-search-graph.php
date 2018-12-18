f7a609b3c319a5231464bfd2431f889a3793ac69
asert <ptim@i.ua>
2018-12-18 13:20:41 +0200

    Create zabbix-search-graph.php

---
+++ zabbix-search-graph.php
@@ -0,0 +1,46 @@
+	$params = [
+			'search' => [
+					'name' => $search
+			],
+			'limit' => $rows_per_page,
+			'selectHosts' => [
+					"hostid",
+					"host",
+					"name"
+			],
+			'output' => ['name', 'graphid','hosts'],
+			'expandName' => true,
+			'templated' => false,
+			'searchByAny' => true
+	];
+	
+	$db_graph = API::Graph()->get($params);
+
+	$viewCount = count($db_graph);
+
+	$table = (new CTableInfo())
+			->setHeader([
+					_('Graphs'),
+					_('Host name'),
+					_('Visible name')
+			]);
+
+	foreach ($db_graph as $tnum => $graph) {
+			if (isset($graph['graphid'])) {
+					$graphs_link = new CLink($graph['name'], 'charts.php?graphid='.$graph['graphid']);
+					$hosts_link = new CLink($graph['hosts'][0]['host'], 'hosts.php?form=update&hostid='.$graph['hosts'][0]['hostid']);
+					$hosts_visible_link = new CLink($graph['hosts'][0]['name'], 'hosts.php?form=update&hostid='.$graph['hosts'][0]['hostid']);
+
+					$table->addRow([
+							$graphs_link,
+							$hosts_link,
+							$hosts_visible_link
+					]);
+			}
+	}
+
+	$widgets[] = (new CCollapsibleUiWidget(WIDGET_SEARCH_TEMPLATES, $table))
+			->setExpanded((bool) CProfile::get('web.search.hats.'.WIDGET_SEARCH_TEMPLATES.'.state', true))
+			->setHeader(_('Graphs'), [], false, 'search.php')
+			->setFooter(new CList([_s('Displaying %1$s of %2$s found', $viewCount, $overalCount)]));
+      
